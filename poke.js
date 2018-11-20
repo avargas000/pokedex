79bc8c7540b002210cfeeaa5bf1cd43853c3c316
Ana Vargas <avargas000@nyc.yearup.org>
2018-11-20 15:27:41 -0500

    done

--- poke.js
+++ poke.js
@@ -1,30 +1,34 @@
 console.log("Poke-pokemon")
 
 class Pokemon {
-  constructor(name,hp,attack, defense, abilities){
+  constructor(name,hp,attack, defense, abilities, statId){
     this.pokeName = name;
     this.Hp = hp;
     this.Attack = attack;
     this.Defense = defense;
     this.Abilities = abilities;
-    this.container = document.querySelector(".charman");
-    this.element =  document.createElement ("div");
-    this.element.className = ("poke");
+
+
+    this.container = document.getElementById(statId);
   }
   display(){
    pokeinfo = document.createElement("ol");
    pokeinfo.innerHTML = [this.pokeName ," HP: " + this.Hp ," Attack: " + this.Attack ," Defense: " + this.Defense ," Abilities: " + this.Abilities];
-   this.element.appendChild(pokeinfo);
-   console.log("pleasework")
-   this.container.appendChild(this.element);
+
+   console.log("pleasework", this)
+   this.container.appendChild(pokeinfo);
   }
 }
+
 
   let pokeinfo;
   function charmander(){
     let charmanderURL ="http://fizal.me/pokeapi/api/v2/name/charmander.json";
     axios.get(charmanderURL)
       .then(function(response){
+
+
+
         let abi = response.data.abilities;
         let all1 = [];
         for(let i=0;i<abi.length;i++){
@@ -44,22 +48,57 @@ class Pokemon {
 
         let defense = response.data.stats[3].base_stat;
 
-        let charr = new Pokemon(name, hp, attack, defense, concatenate);
+        let charr = new Pokemon(name, hp, attack, defense, concatenate, "stats1");
 
-        document.getElementById('image3').style.display = "none";
-        document.getElementById('content3').style.display = "none";
+        // document.getElementById('image3').style.display = "none";
+        // document.getElementById('content3').style.display = "none";
 
         charr.display();
 
      })
   }
 
+  function jiggly(){
+    let jigglyURL ="http://fizal.me/pokeapi/api/v2/name/jigglypuff.json";
+    axios.get(jigglyURL)
+      .then(function(response){
 
-console.log("STFU")
+
+
+        let abi3 = response.data.abilities;
+        let all3 = [];
+        for(let i=0;i<abi3.length;i++){
+          all3.push(abi3[i].ability.name);
+        }
+
+        let concatenate = "";
+        for(let i=0;i<abi3.length;i++){
+          concatenate +=all3[i] + " ";
+        }
+
+        let name = "Jigglypuff";
+
+        let hp = response.data.stats[5].base_stat;
+
+        let attack = response.data.stats[4].base_stat;
+
+        let defense = response.data.stats[3].base_stat;
+
+        let jigglypf = new Pokemon(name, hp, attack, defense, concatenate, "stats3");
+
+        // document.getElementById('image3').style.display = "none";
+        // document.getElementById('content3').style.display = "none";
+
+        jigglypf.display();
+
+     })
+  }
+
+console.log("worked")
 function squirts(){
   let squirtleURL ="http://fizal.me/pokeapi/api/v2/name/squirtle.json";
     axios.get(squirtleURL)
-    .then(function squirts(response){
+    .then(function(response){
 
 
 
@@ -74,7 +113,7 @@ function squirts(){
         concatenate +=all2[i] + " ";
       }
 
-      let name = "squirtle";
+      let name = "Squirtle";
 
       let hp = response.data.stats[5].base_stat;
 
@@ -82,49 +121,19 @@ function squirts(){
 
       let defense = response.data.stats[3].base_stat;
 
-      let squir = new Pokemon(name,hp,attack,defense,concatenate);
+      let squir = new Pokemon(name,hp,attack,defense,concatenate, "stats2");
 
-      document.getElementById('image3').style.display = "none";
-      document.getElementById('image1').style.display = "none";
+      // document.getElementById('image3').style.display = "none";
+      // document.getElementById('image1').style.display = "none";
 
       squir.display();
     })
 }
 
 
-  // function squirts(){
-  //   let squirtleURL ="http://fizal.me/pokeapi/api/v2/name/squirtle.json";
-  //   axios.get(squirtleURL)
-  //     .then(function squirts (response){
-  //       let abi2 = response.data.abilities;
-  //       let all2 = [];
-  //       for(let i=0;i<abi2.length;i++){
-  //         all2.push(abi2[i].ability.name);
-  //
-  //       }
-  //
-  //       let concatenate = "";
-  //       for(let i=0;i<abi2.length;i++){
-  //         concatenate +=all2[i] + " ";
-  //       }
-  //
-  //       let name = "squirtle";
-  //
-  //       let hp = response.data.stats[5].base_stat;
-  //
-  //       let attack = response.data.stats[4].base_stat;
-  //
-  //       let defense = response.data.stats[3].base_stat;
-  //
-  //       let squirt = new Pokemon(name, hp, attack, defense, concatenate);
 
-        // document.getElementById('image3').style.display = "none";
-        // document.getElementById('content3').style.display = "none";
+console.log("worked2")
 
-        // squirt.display();
-console.log("STFU2")
-  //    })
-  // }
 
 
 
@@ -152,7 +161,6 @@ let image2 = document.getElementById('image2');
 button2.addEventListener('click', function(){
   content2.innerText = "SPITTTINGGG WATERRR";
   content2.style.backgroundColor = "lightskyblue";
-  // image2.background-image:url("jigglymuv.png");
   cont.style.backgroundImage = "url('wet.gif')";
   cont.style.backgroundSize = "cover";
   content.style.display = "none"; /*Charmander txt*/
@@ -161,6 +169,7 @@ button2.addEventListener('click', function(){
   image3.style.display = "none"; /*jiggly pic*/
   content3.style.display = "none"; /*jiggly txt*/
   sleep.style.display = "none";
+  squirts();
 })
 let button3 = document.getElementById('sleep');
 let content3 = document.getElementById('content3');
@@ -176,14 +185,14 @@ sleep.addEventListener('click', function(){
   image2.style.display = "none"; /*squirtle pic*/
   button2.style.display = "none";  /*squirtle button*/
   content2.style.display = "none"; /*squirtle txt*/
-  // stats1.style.display = "none";
-  // stats2.style.display = "none";
+  stats1.style.display = "none";
+  stats2.style.display = "none";
+  jiggly();
 });
 // pokedex trainer button
 let change = document.getElementById('pokedexbutton');
 let content4 = document.getElementById('content4');
 let blue = document.getElementById('blue');
-// let container = document.getElementByClassName('container');
 change.addEventListener('click', function(){
   content4.innerText = "OH SNAP!";
   content4.style.backgroundColor = "orange";
@@ -193,74 +202,32 @@ change.addEventListener('click', function(){
   image3.style.display = "inline-block"; /*jiggly pic*/
   button2.style.display = "inline-block";  /*squirtle button*/
   sleep.style.display = "inline-block"; /*jiggly button*/
-  content2.style.display = "inline-block"; /*squirtle txt*/
-  content3.style.display = "inline-block";/*jiggly txt*/
-  content.style.display = "inline-block"; /*Charmander txt*/
+  content2.innerHTML = " "; /*squirtle txt*/
+  content3.innerHTML = " ";/*jiggly txt*/
+  content.innerHTML = " "; /*Charmander txt*/
   image1.style.display = "inline-block"; /*Charmander pic*/
   fire.style.display = "inline-block";  /*Charmander button*/
-  // stats2.style.display = "none";
+  stats1.innerHTML = " ";
+  stats2.innerHTML = " ";
+  stats3.innerHTML = " ";
+
 })
 ;
 
 
+class Trainer {
+  constructor(name, age, gender, talent){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.abilities = talent;
 
-//
-// class Trainer {
-//   get(all)
-// }
-//***************************CHARMANDER********************/////
-// function charResult(){
-// let info1 = document.getElementById('image1');
-// let idk1 = document.getElementById('stats1');
-// let charmander = "http://fizal.me/pokeapi/api/v2/name/charmander.json";
-// axios.get(charmander)
-//   .then(function(response){
-//     let def = response.data.stats["0","3"].base_stat; /*4=attack 3=defense 5=HP*/
-//     idk1.innerHTML = "Attack" + def ;
-//
-//
-//
-//   })
-// }
-// //***************************SQUIRTLE********************/////
-// function squirtResult(){
-// // let info2 = document.getElementById('image2');
-// let idk2 = document.getElementById('stats2');
-// let squirtle = "http://fizal.me/pokeapi/api/v2/name/squirtle.json";
-// axios.get(squirtle)
-//   .then(function(response){
-//     let def2 = response.data.stats[3].base_stat;
-//     idk2.innerHTML = def2;
-//
-//   })
-// }
-// // **************************JIGGLY*************************
-// function jigglyResult(){
-// let info3 = document.getElementById('image3');
-// let idk3 = document.getElementById('stats3');
-// let jigglypuff = "http://fizal.me/pokeapi/api/v2/name/jigglypuff.json";
-// axios.get(jigglypuff)
-//   .then(function(response){
-//     let def3 = response.data.stats[3].base_stat;
-//     idk3.innerHTML = def3;
-//
-//   document.getElementById("fire").addEventListener("click", charResult);
-//   document.getElementById("water").addEventListener("click", squirtResult);
-//   document.getElementById("sleep").addEventListener("click", jigglyResult);
-// })
-// }
+   // console.log(it worked! ALL THE WAY DOWN!);
+}
+}
 
-// class Pokemon {
-//   constructor(Hp,Attack, Defense, Abilities){
-//     this.ClassName = Charmander;
-//     this.Hp = Hp;
-//     this.Attack = Attack;
-//     this.Defense = Defense;
-//     this.Ability = Ability;
-//     this.container = document.getElementByClassName("container");
-//     this.element =  document.createElement ("div");
-//     this.element.className = ("player");
-
+let someone = new Trainer("Blue", 12, "Non-Binary", "Fights stress");
+console.log(someone);
 
 //
 //
